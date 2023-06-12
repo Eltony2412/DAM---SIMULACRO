349570fcf144aef38271fd52ac64d71e1d1cde9a
Dj2412 <1913110155@untels.edu.pe>
2023-06-12 17:37:47 -0500

    DAM - SIMULACRO

---
+++ DAM_ElberthLuquev2/app/src/main/java/com/example/dam_elberthluquev2/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.dam_elberthluquev2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button b_enviar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b_enviar = findViewById(R.id.button_enviar);
+
+        //evento click
+
+        b_enviar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Se envió sus datos", Toast.LENGTH_SHORT).show();
+    }
+}
